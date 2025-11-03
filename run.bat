cb3289350dcbf9bdcafd835fd8b832c9b47254c0
eldek <eduardo.santiagodelfante10@gmail.com>
2025-11-03 11:29:55 -0300

    Se agrego el .bat

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+echo === Compilando Programa ===
+
+set JFLEX_HOME=jflex
+%JFLEX_HOME%\bin\jflex sandwich.flex
+
+java -jar java-cup-11b.jar sandwich.cup
+
+javac -cp java-cup-11b-runtime.jar *.java
+
+echo === Corriendo Recetario ===
+echo.
+
+java -cp .;java-cup-11b-runtime.jar Main sandwich.txt
