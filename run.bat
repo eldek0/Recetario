80c96f13029527aaf6fe0a8d6f7652826a3be143
eldek <es.delfante@gmail.com>
2025-11-05 20:36:51 -0300

    cambio nombre a recetario

--- run.bat
+++ run.bat
@@ -2,15 +2,16 @@
 echo === Compilando Programa ===
 
 set JFLEX_HOME=jflex
+set NAME=recetario
 
 :: Crear carpeta target si no existe
 if not exist target mkdir target
 
 :: Generar lexer con JFlex
-call jflex sandwich.flex
+call jflex %NAME%.flex
 
 :: Generar parser con Cup
-java -jar java-cup-11b.jar sandwich.cup
+java -jar java-cup-11b.jar %NAME%.cup
 
 :: Compilar y colocar .class en target
 javac -cp java-cup-11b-runtime.jar -d target *.java
@@ -19,4 +20,4 @@ echo === Corriendo Recetario ===
 echo.
 
 :: Ejecutar desde la carpeta target
-java -cp target;java-cup-11b-runtime.jar Main sandwich.txt
+java -cp target;java-cup-11b-runtime.jar Main %NAME%.txt
