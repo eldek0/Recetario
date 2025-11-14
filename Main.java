80933bf6cd384872a5be2398c084dc907ad8491e
eldek <es.delfante@gmail.com>
2025-11-14 20:14:18 -0300

    Se arreglo un problema

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ public class Main {
     /* Start the parser */
     try {
       parser p = new parser(new Lexer(new FileReader(argv[0])));
-      Object result = p.parse().value;      
+      Object result = p.parse().value; 
+      
+      Integer sum = 0;
+      for (Integer f : p.table.values()) {
+          sum += f;
+      }
+      System.out.println("total pedidos: " + sum);
     } catch (Exception e) {
       /* do cleanup here -- possibly rethrow e */
       e.printStackTrace();
